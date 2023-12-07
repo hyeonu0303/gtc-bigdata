748674c21578e727845c1fc558016bff99ab805f
hyeonu <hyeonu030334@gmail.com>
2023-12-07 22:47:39 +0900

    fix: error main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Test from './test.jsx'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element:<App/>
-  },
-  {
-    path: "/test",
-    element: <Test/>,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      
-    </RouterProvider>
+    <App/>
   </React.StrictMode>,
 )
